93d35ad86de97bbec8a300264ec8fc2f519d1cd1
Brenda309 <brewihogora23@gmail.com>
2022-09-09 23:42:41 +0200

    Add tests for functions

--- rspec/solver_spec.rb
+++ rspec/solver_spec.rb
@@ -7,11 +7,11 @@ describe Solver do
 it 'should show a solver object is an instance of solver class' do
       expect(sv).to be_instance_of Solver
     end
-it 'should reverse string when reverse method is called' do
-      expect(sv.reverse('lion')).to eq('noil')
+it 'should reverse string' do
+      expect(sv.reverse('guy')).to eq('yug')
       expect(sv.reverse('hello')).to eq('olleh')
     end
-  it 'should return fizz, buzz, fizzbuzz or string of an integer when fizzbuzz method is called' do
+  it 'should return fizz, buzz, fizzbuzz or string of an integer' do
       expect(sv.fizzbuzz(3)).to eq('fizz')
       expect(sv.fizzbuzz(5)).to eq('buzz')
       expect(sv.fizzbuzz(30)).to eq('fizzbuzz')
